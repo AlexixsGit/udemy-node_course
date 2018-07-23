1bedab1baed98504475b84c2933fc029b349c390
Edward Ortiz <edaortiz@bancolombia.com.co>
2018-07-22 21:19:43 -0500

    user model created

---
+++ server/model/user.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+let Schema = mongoose.Schema;
+
+let UserSchema = new Schema({
+    name: {
+        type: String,
+        require: [true, 'The name is required']
+    },
+    email: {
+        type: String,
+        required: [true, 'The email is required']
+    },
+    password: {
+        type: String,
+        required: [true, 'The password is required']
+    },
+    img: {
+        type: String
+    },
+    role: {
+        type: String,
+        default: 'User_role'
+    },
+    status: {
+        type: Boolean
+    }, google: {
+        type: Boolean
+    }
+});
+
+module.exports = mongoose.model('User', UserSchema);
